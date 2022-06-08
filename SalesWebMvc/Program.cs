5124f20f26a1fdbe9a6b8bbedf8f9128ae9825d1
Paulo Teodoro <pteodoro19@gmail.com>
2022-06-08 18:24:25 -0300

    App locale, number and date formatting

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Localization;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+using System.Globalization;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +31,16 @@ if (!app.Environment.IsDevelopment())
 
 var seedSrv = builder.Services.BuildServiceProvider().GetService<SeedingService>();
 seedSrv.Seed();
+
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+};
+
+app.UseRequestLocalization(localizationOptions);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
